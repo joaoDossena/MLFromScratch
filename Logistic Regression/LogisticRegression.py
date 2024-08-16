d73efccda203d850c525b7918c174785966c2a99
joaoDossena <joaopdossena@gmail.com>
2024-08-16 16:52:22 -0300

    Adding Logistic Regression

---
+++ Logistic Regression/LogisticRegression.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def sigmoid(x):
+    x = np.clip(x, -500, 500)
+    return 1/(1 + np.exp(-x))
+
+class LogisticRegression:
+
+    def __init__(self, lr=0.001, n_iters=1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+    
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.n_iters):
+            linear_predictions = np.dot(X, self.weights) + self.bias
+            predictions = sigmoid(linear_predictions)
+            
+            dw = (1/n_samples) * np.dot(X.T, (predictions - y))
+            db = (1/n_samples) * np.sum(predictions - y)
+
+            self.weights = self.weights - self.lr * dw
+            self.bias = self.bias - self.lr * db
+
+
+    def predict(self, X):
+        linear_predictions = np.dot(X, self.weights) + self.bias
+        y_preds = sigmoid(linear_predictions)
+        return [0 if y < 0.5 else 1 for y in y_preds]
+            
+
+
