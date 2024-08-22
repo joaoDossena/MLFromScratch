3b7bf5084cf54048adc36615d238c9e6c662d87b
joaoDossena <joaopdossena@gmail.com>
2024-08-22 16:45:15 -0300

    Adding Naive Bayes algorithm

---
+++ 06 - Naive Bayes/train_naive_bayes.py
@@ -0,0 +1,29 @@
+
+
+# Testing
+if __name__ == "__main__":
+    
+
+    from sklearn.model_selection import train_test_split
+    from sklearn import datasets
+    import numpy as np
+    from NaiveBayes import NaiveBayes
+
+    def accuracy(y_true, y_pred):
+        return np.sum(y_true==y_pred) / len(y_true)
+
+    X, y = datasets.make_classification(
+        n_samples=1000, n_features=10, n_classes=2, random_state=123
+    )
+
+    X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=123
+    )
+
+    nb = NaiveBayes()
+    nb.fit(X_train, y_train)
+    preds = nb.predict(X_test)
+
+    print(f"Accuracy: {accuracy(y_test, preds)}")
+
+
